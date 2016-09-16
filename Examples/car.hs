3f7d6d69d98a6a72a75fd3318027e17df33f7d97
Mark Grebe <mark.grebe@ku.edu>
2016-09-16 09:46:43 -0500

    Add car.hs

---
+++ Examples/car.hs
@@ -0,0 +1,7 @@
+data Car = Car { company :: String
+               , model :: String
+               , year :: Int
+               } deriving Show
+
+c = Car {company="Chevy", model="Camero", year=1970}
+
