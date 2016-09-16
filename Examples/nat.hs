7b785f73752ecafcc526cd701073a1e515eb30b1
Mark Grebe <mark.grebe@ku.edu>
2016-09-16 09:48:29 -0500

    Fixed newtype slides

--- Examples/nat.hs
+++ Examples/nat.hs
@@ -1,6 +1,12 @@
 data Nat = Zero | Succ Nat
   deriving Show
 
+myShow :: Nat -> String
+myShow Zero = "Zero"
+myShow (Succ n) = case n of
+    Zero -> "Succ Zero"
+    otherwise -> "Succ (" ++ myShow n ++ ")"
+
 int2nat  :: Int -> Nat
 int2nat 0 = Zero
 int2nat n = Succ (int2nat (n-1))
